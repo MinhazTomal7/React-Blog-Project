e8d692cef3440f608831c51593e00f480b9eb976
Minhaz Tomal <aminminhaz55@gmail.com>
2025-01-09 00:40:31 +0600

    Upto Master Layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = (props) => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar shadow bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -39,20 +39,7 @@ const Layout = (props) => {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
                     </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
                 </div>
             </div>
             {props.children}
